5a8a041aca0e5bbcbab7b194e7c5963d892ca9e1
AnaRami9595 <anaramirezvazquez9595@gmail.com>
2023-08-31 19:51:47 -0600

    Episode 04 assignment

--- Assigments/Episode 01/assigment.js
+++
@@ -1,7 +0,0 @@
-/*import React from "react";
-import ReactDOM  from "react-dom";
-
-const reactEl = React.createElement("h1", {}, "I'm an h1 tag");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(reactEl)
-*/
---
+++ Assignments/Episode 03/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import ReactDOM from "react";
+import Header from "./Header"
+/*📝Instructions:
+
+1. Create a Nested Header Element using React.createElement
+ (h1, h2 h3 inside a div with a class named "title")
+
+2. Create a functional component of the same with JSX
+
+3. Pass attributes into the tag in JSX
+
+4. Make Composition of component (add component inside another)
+
+5. Create a Header Component from scratch using Functional
+Components with JSX
+
+    - Add logo on the left
+    - Add a search bar in the middle
+    - Add User icon on the right
+    - Ad Css to make it look nice
+
+*/
+
+
+//1️⃣Nester Header with createElEMENT
+
+const parent = React.createElement("div", {className: "title"},[
+    React.createElement("h1", {}, "Hello"),
+    React.createElement("h2", {}, "Hello"),
+    React.createElement("h3", {}, "Hello"),
+    ]);
+
+//root.render(parent)//This works well ✅
+
+//2️⃣Nester Header with JSX🍤 with Functional Component🧩
+//3️⃣Attributes inside tag:
+
+const JsxHeader = () => {
+    return ( 
+        <div className = "title">
+            <h1 className="red">Hello</h1>
+            <h2 className="white">Hello</h2>
+            <h3 className="blue">Hello</h3>
+        </div>
+    );
+}
+
+//1️⃣Component🧩 Composition
+
+const CompositionComponent = () => {
+    return ( <div>
+        <JsxHeader/>
+    </div> );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Header/>)
+ 
+
+
